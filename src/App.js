def8f532c7b8719b31604e053eb01cdbd533fd3d
지영 <easy@ijiyeong-ui-MacBook-Pro.local>
2019-09-22 09:23:32 +0900

    Add Navigator image Add menu, logo, setting image src for navigator bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./App.css";
+import Navigator from "./navigator/Navigator";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Lady Bug Project</p>
-      </header>
+      <Navigator />
     </div>
   );
 }
--- src/navigator/Navigator.js
+++ src/navigator/Navigator.js
@@ -1,10 +1,17 @@
 import React from "react";
+import menu_icon from "../img/menu_icon.png";
+import logo_icon from "../img/logo_icon.png";
+import setting_icon from "../img/setting_icon.png";
 
 class Navigator extends React.Component {
   render() {
     return (
       <div className="navigator">
-        <div className="navigator-logo"></div>
+        <div className="navigator-logo">
+          <img src={menu_icon} alt="menu" />
+          <img src={logo_icon} alt="logo" />
+          <img src={setting_icon} alt="setting" />
+        </div>
         <div className="navigator-button"></div>
       </div>
     );
