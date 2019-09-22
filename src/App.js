ef8071d6f9bd921cf211cee05b01fcf15a942e1e
지영 <easy@ijiyeong-ui-MacBook-Pro.local>
2019-09-22 16:24:44 +0900

    Add recommend title div to recomment Fix recommend css file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navigator from "./navigator/Navigator";
+import Recommend from "./recommend/Recommend";
 
 function App() {
   return (
     <div className="App">
       <Navigator />
+      <Recommend />
     </div>
   );
 }
---
+++ src/recommend/Recommend.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./recommend.css";
+
+class Recommend extends React.Component {
+  render() {
+    return (
+      <div className="wrap-recommend">
+        <div className="recommend-title">어머! 이건 꼭 봐야해!</div>
+        <div className="recommend-slider"></div>
+      </div>
+    );
+  }
+}
+
+export default Recommend;
