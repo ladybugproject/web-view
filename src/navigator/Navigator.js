65f9b55160289e59011780fa0c54ac2cd0cb4fee
지영 <easy@ijiyeong-ui-MacBook-Pro.local>
2019-09-22 09:50:02 +0900

    Add Navigator CSS Add home and detail search button to Navigator

--- src/navigator/Navigator.js
+++ src/navigator/Navigator.js
@@ -2,17 +2,25 @@ import React from "react";
 import menu_icon from "../img/menu_icon.png";
 import logo_icon from "../img/logo_icon.png";
 import setting_icon from "../img/setting_icon.png";
+import "./Navigator.css";
 
 class Navigator extends React.Component {
   render() {
     return (
       <div className="navigator">
         <div className="navigator-logo">
-          <img src={menu_icon} alt="menu" />
-          <img src={logo_icon} alt="logo" />
-          <img src={setting_icon} alt="setting" />
+          <img className="navigator-menu-icon" src={menu_icon} alt="menu" />
+          <img className="navigator-logo-icon" src={logo_icon} alt="logo" />
+          <img
+            className="navigator-setting-icon"
+            src={setting_icon}
+            alt="setting"
+          />
         </div>
-        <div className="navigator-button"></div>
+        <div className="navigator-button">
+          <div className="navigator-home-button">Home</div>
+          <div className="navigator-detail-search-button">Search</div>
+        </div>
       </div>
     );
   }
