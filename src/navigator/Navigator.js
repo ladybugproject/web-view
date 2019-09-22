4377904a48ddf455a82413c781018ab2c796e9b1
지영 <easy@ijiyeong-ui-MacBook-Pro.local>
2019-09-22 09:11:19 +0900

    Add Navigator component folder and file

---
+++ src/navigator/Navigator.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Navigator extends React.Component {
+  render() {
+    return (
+      <div className="navigator">
+        <div className="navigator-logo"></div>
+        <div className="navigator-button"></div>
+      </div>
+    );
+  }
+}
+
+export default Navigator;
