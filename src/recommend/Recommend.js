af3bd73ecfcac53fd9a8b2c312633ad8556eb263
EasyZero <eminem54@naver.com>
2019-09-28 21:17:35 +0900

    수정: concert 아이템 배열로 생성하도록 변경
    아이템컨테이너에 들어가는 공연들 배열로 만들어서 삽입
    코드가 너무 길어져서 수정
    추가로 이벤트핸들러 만들기 위해 mjs파일생성
    css 오버플로우 hidden으로 수정
    아이템 하나당 width 100% 차지하게 변경

--- src/recommend/Recommend.js
+++ src/recommend/Recommend.js
@@ -15,36 +15,51 @@ class Recommend extends React.Component {
         this.setState({ concert: data });
       });
   }
+
+  makeConcertElements(concert) {
+    const [status, ...concerts] = concert;
+    const concertElements = []
+    for (const data of concerts) {
+      const url = data.POSTER;
+      const title = data.PRFNM
+      const location = data.FCLTYNM;
+      const dateStart = data.PRFPDFROM;
+      const dateFinish = data.PRFPDTO;
+      const actor = data.PRFCAST;
+
+      concertElements.push((
+        <div className="wrap-recommend-item">
+          <img
+            className="recommend-item-poster"
+            src={url}
+            alt="sample"
+          />
+          <div className="recommend-item-info">
+            <div className="recommend-item-title">{title}</div>
+            <div className="recommend-item-location">
+              {location}
+            </div>
+            <div className="recommend-item-date">
+              {dateStart} ~ {dateFinish}
+            </div>
+            <div className="recommend-item-actor">{actor}</div>
+          </div>
+        </div>
+    ));
+    }
+    return concertElements;
+  }
   render() {
     const { concert } = this.state;
     if (concert.length === 0) return <div></div>;
-    const url = concert[1].POSTER;
-    const title = concert[1].PRFNM
-    const location = concert[1].FCLTYNM;
-    const dateStart = concert[1].PRFPDFROM;
-    const dateFinish = concert[1].PRFPDTO;
-    const actor = concert[1].PRFCAST;
+    
+    const concertElements = this.makeConcertElements(concert);
+
     return (
       <div className="wrap-recommend">
         <div className="recommend-title">어머! 이건 꼭 봐야해!</div>
         <div className="recommend-slider">
-          <div className="wrap-recommend-item">
-            <img
-              className="recommend-item-poster"
-              src={url}
-              alt="sample"
-            />
-            <div className="recommend-item-info">
-              <div className="recommend-item-title">{title}</div>
-              <div className="recommend-item-location">
-                {location}
-              </div>
-              <div className="recommend-item-date">
-                {dateStart} ~ {dateFinish}
-              </div>
-              <div className="recommend-item-actor">{actor}</div>
-            </div>
-          </div>
+          {concertElements}
         </div>
       </div>
     );
