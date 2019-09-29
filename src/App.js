cb7b1dee9ce3f530d59e53023bd6ffcd69b7accb
EasyZero <eminem54@naver.com>
2019-09-29 23:33:45 +0900

    수정: App.js에 라우터 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Navigator from "./navigator/Navigator";
 import Recommend from "./recommend/Recommend";
 import SearchBar from "./searchBar/SearchBar";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends React.Component {
   render() {
