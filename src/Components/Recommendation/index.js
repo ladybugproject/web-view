917688dc2f108fea20af8536b998a58cf2706c8a
seokhyun-oh <seokhyun.oh@linecorp.com>
2019-10-25 18:41:04 +0900

    Add recommendation item to force scroll

--- src/Components/Recommendation/index.js
+++ src/Components/Recommendation/index.js
@@ -4,7 +4,6 @@ import './Recommendation.css';
 
 class TouchScrollTracker {
 
-  static MIN_SCROLL_THRESHOLD = 20;
   static SCROLL_TO_LEFT = -1;
   static NO_SCROLL = 0;
   static SCROLL_TO_RIGHT = 1;
@@ -23,13 +22,17 @@ class TouchScrollTracker {
     const delta = toX - this.fromX;
     const direction = this.getDirection(delta);
 
+    if (direction === TouchScrollTracker.NO_SCROLL) {
+      return;
+    }
+
     this.target.scroll.call(this, direction);
   }
 
   getDirection = delta => {
     const absDelta = Math.abs(delta);
 
-    if (absDelta < TouchScrollTracker.MIN_SCROLL_THRESHOLD) {
+    if (absDelta === 0) {
       return TouchScrollTracker.NO_SCROLL;
     }
 
@@ -60,6 +63,14 @@ class Recommendation extends React.Component {
           date_to: '2019-01-06',
           cast: '하은설, 김주연, 조상웅, 노희찬, 장이주, 조민정, 이세령 등',
         },
+        {
+          prf_name: '판타스틱',
+          poster: 'http://www.kopis.or.kr/upload/pfmPoster/PF_PF121653_180607_112000.jpg',
+          plc_name: 'NH아트홀 판타스틱 전용관 (NH아트홀 판타스틱 전용관)',
+          date_from: '2009-04-25',
+          date_to: '2019-09-30',
+          cast: '백승우, 양대용, 문진태, 김태연, 손힘찬, 박현수, 어하연 등',
+        },
       ],
     };
     this.itemIndex = 0;
@@ -74,7 +85,13 @@ class Recommendation extends React.Component {
     }
 
     this.itemIndex = nextItemIndex;
-    console.log(this.element);
+
+    const scrollToX = this.itemIndex * this.element.clientWidth;
+
+    this.element.scrollTo({
+      left: scrollToX,
+      behavior: 'smooth',
+    });
   };
 
   render() {
@@ -136,6 +153,33 @@ class Recommendation extends React.Component {
                 <span className={'content-key'}>출연진</span>
                 <span>
                   {this.state.recommendations[1].cast}
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className={'recommendation-item'}>
+            <img className={'poster'} src={this.state.recommendations[0].poster} alt={''}/>
+            <div className={'descriptions'}>
+              <div className={'description prf-name'}>
+                {this.state.recommendations[2].prf_name}
+              </div>
+              <div className={'description'}>
+                <span className={'content-key'}>장소</span>
+                <span>
+                  {this.state.recommendations[2].plc_name}
+                </span>
+              </div>
+              <div className={'description'}>
+                <span className={'content-key'}>날짜</span>
+                <span>
+                  {this.state.recommendations[2].date_from} ~ {this.state.recommendations[0].date_to}
+                </span>
+              </div>
+              <div className={'description'}>
+                <span className={'content-key'}>출연진</span>
+                <span>
+                  {this.state.recommendations[2].cast}
                 </span>
               </div>
             </div>
