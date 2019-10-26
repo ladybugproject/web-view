82094ee2bad9f5ed9c7318bafaf7739fe2b0cfcd
지영 <easy@ijiyeong-ui-MacBook-Pro.local>
2019-10-26 16:59:46 +0900

    추가: 상세검색용 파일 추가
    네비게이터와 필터를 반환

---
+++ src/Components/DetailSearch/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigator from '../Navigator';
+import Filter from './Filter';
+
+const DetailSearch = () => {
+	return (
+		<>
+			<Navigator />
+			<Filter />
+		</>
+	);
+};
+
+export default DetailSearch;
