3bc1f9201654e679c5f47aff929a84056f500726
지영 <easy@ijiyeong-ui-MacBook-Pro.local>
2019-10-26 15:59:31 +0900

    추가: 필터 컴포넌트 추가
    필터 기능 추가할 wrapper추가
    홈에서 상세검색으로 라우팅예정

---
+++ src/Components/Filter/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const WrapFilter = styled.div`
+  display: flex;
+`;
+
+const Filter = () => {
+  return (
+    <div>
+      <WrapFilter></WrapFilter>
+    </div>
+  );
+};
+
+export default Filter;
