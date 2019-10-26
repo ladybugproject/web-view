9881f061e15b2661a9c1f876b1a8edee9d2d1a34
지영 <easy@ijiyeong-ui-MacBook-Pro.local>
2019-10-26 16:20:54 +0900

    추가: 홈버튼, 상세검색버튼에 앵커추가
    Link사용해서 라우팅으로 이동되도록 react-router-dom link사용

--- src/Components/Navigator/index.js
+++ src/Components/Navigator/index.js
@@ -1,25 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Setup from './setup.svg';
 import Logo from './logo.png';
 import Settings from './settings.svg';
 import './Navigator.css';
 
 class Navigator extends React.Component {
-  render() {
-    return (
-      <div className={'navigator'}>
-        <div className={'menu-icon'}>
-          <img className={'navigator-menu-icon left'} src={Setup}  alt={Setup} />
-          <img className={'center'} src={Logo}  alt={Logo} />
-          <img className={'navigator-menu-icon right'} src={Settings}  alt={Setup} />
-        </div>
-        <div className={'navigator-menu-btn'}>
-          <button className={'navigator-btn selected'}>HOME</button>
-          <button className={'navigator-btn'}>상세검색</button>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className={'navigator'}>
+				<div className={'menu-icon'}>
+					<img className={'navigator-menu-icon left'} src={Setup} alt={Setup} />
+					<img className={'center'} src={Logo} alt={Logo} />
+					<img className={'navigator-menu-icon right'} src={Settings} alt={Setup} />
+				</div>
+				<div className={'navigator-menu-btn'}>
+					<Link to="/">
+						<button className={'navigator-btn selected'}>HOME</button>
+					</Link>
+					<Link to={'/detail'}>
+						<button className={'navigator-btn'}>상세검색</button>
+					</Link>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Navigator;
