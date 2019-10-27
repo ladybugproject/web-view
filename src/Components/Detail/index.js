14de9f7122c46683d863185bbcf04220cffaac52
seokhyun-oh <seokhyun.oh@linecorp.com>
2019-10-27 16:49:32 +0900

    Add synopsis component

--- src/Components/Detail/index.js
+++ src/Components/Detail/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navigator from '../Navigator';
 import Overview from './Overview';
+import Synopsis from './Synopsis';
 
 class Detail extends React.Component {
   constructor(props) {
@@ -15,6 +16,7 @@ class Detail extends React.Component {
         date_from: '2009-04-25',
         date_to: '2019-09-30',
         genre: '뮤지컬',
+        synopsis: '작가의 꿈을 안고 상경한 경상도 여자 정은과 정체를 알 수 없는 미스터리 서울 남자 경민이 옥탑방으로 동시에',
         cast: '백승우, 양대용, 문진태, 김태연, 손힘찬, 박현수, 어하연 등',
       },
     };
@@ -27,6 +29,7 @@ class Detail extends React.Component {
       <>
         <Navigator />
         <Overview result={result} />
+        <Synopsis result={result} />
       </>
     );
   }
