e8b745e5a69dc38834a9a96b2691359d2e48a40b
EasyZero <eminem54@naver.com>
2019-10-27 19:49:12 +0900

    수정: App.js 에 상세검색 route 추가

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
-import Home from './Components/Home';
-import GlobalStyles from './GlobalStyles';
+import Home from "./Components/Home";
+import DetailSearch from "./Components/DetailSearch";
+import GlobalStyles from "./GlobalStyles";
 
 class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Router>
-          <Route exact path="/" component={Home} />
-        </Router>
-        <GlobalStyles />
-      {/*<Router>*/}
-        {/*<Navigator />*/}
-        {/*<Route exact path="/" Component={App}>*/}
-          {/*<SearchBar />*/}
-          {/*<Recommend />*/}
-        {/*</Route>*/}
+	render() {
+		return (
+			<>
+				<Router>
+					<Route exact path="/" component={Home} />
+					<Route path="/detail" component={DetailSearch} />
+				</Router>
+				<GlobalStyles />
+				{/*<Router>*/}
+				{/*<Navigator />*/}
+				{/*<Route exact path="/" Component={App}>*/}
+				{/*<SearchBar />*/}
+				{/*<Recommend />*/}
+				{/*</Route>*/}
 
-        {/*<Route path="/information" Component={App}>*/}
-          {/*<SearchBar />*/}
-          {/*<Information />*/}
-        {/*</Route>*/}
-        {/*</Router>*/}
-      </>
-    )
-  }
+				{/*<Route path="/information" Component={App}>*/}
+				{/*<SearchBar />*/}
+				{/*<Information />*/}
+				{/*</Route>*/}
+				{/*</Router>*/}
+			</>
+		);
+	}
 }
 
 export default App;
