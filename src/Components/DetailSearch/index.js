fe56993d10b1042e18126208e643d6be71e2d281
EasyZero <eminem54@naver.com>
2019-10-27 21:31:19 +0900

    수정: 상세검색 클릭시 초기화면용 데이터 불러오도록 수정

--- src/Components/DetailSearch/index.js
+++ src/Components/DetailSearch/index.js
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
-import Navigator from '../Navigator';
-import Filter from './Filter';
-import Concerts from './Concerts';
+import React, { useState, useEffect } from "react";
+import Navigator from "../Navigator";
+import Filter from "./Filter";
+import Concerts from "./Concerts";
+import APIClient from "../APIClient";
 
 const DetailSearch = () => {
 	const [concerts, setConcerts] = useState([]);
+	const fetchInitialData = async () => {
+		const data = await APIClient.prfinfo.search({
+			keyword: "",
+			startFrom: 0,
+			endTo: 20
+		});
+
+		setConcerts(data);
+	};
+	useEffect(() => {
+		fetchInitialData();
+	}, []);
 	return (
 		<>
 			<Navigator />
