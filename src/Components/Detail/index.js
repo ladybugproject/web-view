061349dc4ae6a1f7717893e9160f665ef5f4dab6
seokhyun-oh <seokhyun.oh@linecorp.com>
2019-10-27 17:39:05 +0900

    Add cast component

--- src/Components/Detail/index.js
+++ src/Components/Detail/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navigator from '../Navigator';
 import Overview from './Overview';
 import Synopsis from './Synopsis';
+import Cast from './Cast';
 
 class Detail extends React.Component {
   constructor(props) {
@@ -18,6 +19,10 @@ class Detail extends React.Component {
         genre: '뮤지컬',
         synopsis: '작가의 꿈을 안고 상경한 경상도 여자 정은과 정체를 알 수 없는 미스터리 서울 남자 경민이 옥탑방으로 동시에',
         cast: '백승우, 양대용, 문진태, 김태연, 손힘찬, 박현수, 어하연 등',
+        cast_profile: [
+          { name: "오세미", profile: "http://www.kopis.or.kr/upload/pfmPoster/PF_PF152946_190812_160950.gif" },
+          { name: "유민정", profile: "http://www.kopis.or.kr/upload/pfmPoster/PF_PF152946_190812_160950.gif" },
+        ],
       },
     };
   }
@@ -30,6 +35,7 @@ class Detail extends React.Component {
         <Navigator />
         <Overview result={result} />
         <Synopsis result={result} />
+        <Cast result={result} />
       </>
     );
   }
