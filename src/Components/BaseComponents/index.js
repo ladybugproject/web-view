18841f180821f6b31a3ab543d09566a05b79a35c
EasyZero <eminem54@naver.com>
2019-11-01 19:57:58 +0900

    리팩토링: 여러 곳에서 사용되는 컴포넌트 베이스 리팩토링
    로그인 폼에 사용되는 인풋 래퍼와 이미지 등 추출

--- src/Components/BaseComponents/index.js
+++ src/Components/BaseComponents/index.js
@@ -1,13 +1,54 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Flexbox = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: 100%;
-	height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
 `;
 
 export const FlexDirectionColumn = styled(Flexbox)`
-	flex-direction: column;
+  flex-direction: column;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 40px;
+  outline: none;
+  margin-left: 10px;
+  font-size: 16px;
+  border: none;
+`;
+
+export const InputImg = styled.img`
+  width: 10%;
+  height: 35px;
+  margin-left: 3px;
+  opacity: 0.5;
+`;
+
+export const WrapInput = styled(Flexbox)`
+  background-color: white;
+  width: 288px;
+  height: 50px;
+  margin: 5px auto;
+`;
+
+export const Btn = styled.div`
+  background-color: #7cc5ae;
+  width: 288px;
+  height: 50px;
+  margin-bottom: 10px;
+  font-size: 20px;
+  font-weight: 350;
+  text-align: center;
+  line-height: 50px;
+  box-shadow: 1px 1px 25 grey;
+  border-radius: 0.3rem;
+  color: white;
+  margin: 5px auto;
+  :active {
+    background-color: #7cc5ae;
+  }
 `;
