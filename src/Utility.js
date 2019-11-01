1977d3892f68838cf205f477bf0bed119c3db29c
EasyZero <eminem54@naver.com>
2019-11-01 20:08:33 +0900

    추가: custom fetch 함수 추가
    메서드 타입으로 데이터와 함께 보낼 수 있도록 fetch 함수 구현

--- src/Utility.js
+++ src/Utility.js
@@ -1,6 +1,21 @@
-const $ = (name, home=document) => home.querySelector(name);
-const $$ = (name, home=document) => home.querySelectorAll(name);
+const $ = (name, home = document) => home.querySelector(name);
+const $$ = (name, home = document) => home.querySelectorAll(name);
 
-export {
-  $, $$
+async function fetchUsingMethod(url, { methodType, data }) {
+  const response = await fetch(url, {
+    method: methodType,
+    mode: "cors",
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json"
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrer: "no-referrer", // no-referrer, *client
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response;
 }
+
+export { $, $$, fetchUsingMethod };
